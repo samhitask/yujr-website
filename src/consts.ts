131fb44ca53dc8820f9497b80f70c12c7abc3bba
samhitask <97186723+samhitask@users.noreply.github.com>
2025-05-25 12:13:18 -0700

    feat: site v1
    First version of YURJ site with placeholder blog posts.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Yale Undergraduate Journal of Religion';
+export const SITE_DESCRIPTION = 'A student-run publication dedicated to showcasing excellence in undergraduate research in religious studies';
