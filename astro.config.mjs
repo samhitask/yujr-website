ed3b06956228db2bd73f3bff90c279b0b6d2ef49
samhitask <97186723+samhitask@users.noreply.github.com>
2026-02-04 23:09:01 -0500

    fix: updated netlify configuration to support decap CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-  adapter: vercel(),
+  integrations: [mdx(), sitemap()]
 });
